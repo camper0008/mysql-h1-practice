823412bb307d2df385e807f82cd0a4567130fe10
camper0008 <tphollebeek@gmail.com>
2022-04-29 09:24:49 +0200

    names

--- insert-opg00.sql
+++ insert-opg00.sql
@@ -10,10 +10,10 @@ INSERT INTO opg00 (id, name, age)
 VALUES
 (1,  "dietrich", 127),
 (2,  "dieter",   255),
-(3,  "dddd",     50),
-(4,  "eeeee",    40),
+(3,  "donald",   50),
+(4,  "erik",     40),
+(4,  "erik",     50),
 (5,  "dimon",    40),
 (6,  "dietz",    49),
 (7,  "deeeee",   40),
-(8,  "dennis",   254),
-(9,  "dddd",     50);
+(8,  "dennis",   254);
