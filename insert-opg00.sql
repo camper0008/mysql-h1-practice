6dc6b0b3396de05a73ddc1385b8eba716ae10009
camper0008 <tphollebeek@gmail.com>
2022-04-29 08:52:24 +0200

    refactor

--- insert-opg00.sql
+++ insert-opg00.sql
@@ -2,35 +2,18 @@ DROP TABLE IF EXISTS opg00;
 
 CREATE TABLE opg00 (
    id INT UNSIGNED UNIQUE PRIMARY KEY,
-   name TINYINT,
+   name TINYTEXT,
    age TINYINT UNSIGNED
 );
 
-SELECT * FROM opg00;
-
 INSERT INTO opg00 (id, name, age)
-VALUES (1, "dietrich", 127);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (2, "dieter", 255);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (3, "dddd", 50);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (4, "eeeee", 40);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (5, "dimon", 40);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (6, "dietz", 49);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (7, "deeeee", 40);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (8, "dennis", 254);
-
-INSERT INTO opg00 (id, name, age)
-VALUES (9, "dddd", 50);
+VALUES
+(1,  "dietrich", 127),
+(2,  "dieter",   255),
+(3,  "dddd",     50),
+(4,  "eeeee",    40),
+(5,  "dimon",    40),
+(6,  "dietz",    49),
+(7,  "deeeee",   40),
+(8,  "dennis",   254),
+(9,  "dddd",     50);
