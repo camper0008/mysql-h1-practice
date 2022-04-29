5cfda6b63a34434207146a8d1bb8eb7d411d64ad
camper0008 <tphollebeek@gmail.com>
2022-04-29 09:23:04 +0200

    lowercase & average age

--- opg00.sql
+++ opg00.sql
@@ -5,26 +5,30 @@ ORDER BY Id;
 -- select distinct ages where name starts with d, 
 -- is either >= 50 or < 45
 -- and is not a multiple of 127
-SELECT DISTINCT Age FROM opg00 
-WHERE Name LIKE "D%" 
-AND (Age >= 50 OR Age < 45)
-AND (Age % 127) <> 0;
+SELECT DISTINCT age FROM opg00 
+WHERE name LIKE "D%" 
+AND (age >= 50 OR age < 45)
+AND (age % 127) <> 0;
 
 -- select people with duplicate ages
-SELECT Age, COUNT(Age) FROM opg00 
-GROUP BY Age
-HAVING COUNT(Age) > 1;
+SELECT age, COUNT(age) FROM opg00 
+GROUP BY age
+HAVING COUNT(age) > 1;
 
 -- select * from people with duplicate ages
 SELECT * FROM opg00
-WHERE Age IN (
-  SELECT t.Age FROM (
-    SELECT Age, COUNT(Age) FROM opg00 
-    GROUP BY Age
-    HAVING COUNT(Age) > 1
+WHERE age IN (
+  SELECT t.age FROM (
+    SELECT age, COUNT(age) FROM opg00 
+    GROUP BY age
+    HAVING COUNT(age) > 1
   ) AS t
 );
 
 -- rank people by how long their names are
-SELECT *, LENGTH(Name) FROM opg00
-ORDER BY LENGTH(Name) DESC;
+SELECT *, LENGTH(name) FROM opg00
+ORDER BY LENGTH(name) DESC;
+
+-- average age of people with a certain name
+SELECT name, AVG(age) FROM opg00
+GROUP BY name
