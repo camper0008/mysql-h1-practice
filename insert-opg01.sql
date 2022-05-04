1a3fa99e04ae6c37a53b163a8fe13a82e7b0a4f7
camper0008 <tphollebeek@gmail.com>
2022-05-04 14:49:56 +0200

    view

--- insert-opg01.sql
+++ insert-opg01.sql
@@ -7,22 +7,9 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-INSERT INTO `opg00` (`id`, `name`, `age`) VALUES
-(1,	'dietrich',	127),
-(2,	'dieter',	255),
-(3,	'dddd',	50),
-(4,	'eeeee',	40),
-(5,	'dimon',	40),
-(6,	'dietz',	49),
-(7,	'deeeee',	40),
-(8,	'dennis',	254),
-(9,	'dddd',	50),
-(10,	'dennis',	233),
-(11,	'donald',	18);
-
 DROP TABLE IF EXISTS `opg01_comments`;
 CREATE TABLE `opg01_comments` (
-  `id` int NOT NULL,
+  `id` int unsigned NOT NULL,
   `content` text NOT NULL,
   `FK_post` int NOT NULL,
   `FK_user` int NOT NULL,
@@ -34,15 +21,13 @@ CREATE TABLE `opg01_comments` (
   CONSTRAINT `opg01_comments_ibfk_2` FOREIGN KEY (`FK_user`) REFERENCES `opg01_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `opg01_comments` (`id`, `content`, `FK_post`, `FK_user`) VALUES
-(0,	'har du fået lov af helledie til at lave den post?',	0,	1),
-(1,	'@soelberg\r\nshjøøøør\r\nMvh Dietz',	0,	0);
 
 DROP TABLE IF EXISTS `opg01_friends`;
 CREATE TABLE `opg01_friends` (
   `id` int unsigned NOT NULL,
-  `FK_user0` int NOT NULL,
-  `FK_user1` int NOT NULL,
+  `FK_user0` int unsigned NOT NULL,
+  `FK_user1` int unsigned NOT NULL,
+  UNIQUE KEY `id` (`id`),
   KEY `user0` (`FK_user0`),
   KEY `user1` (`FK_user1`),
   CONSTRAINT `opg01_friends_ibfk_1` FOREIGN KEY (`FK_user0`) REFERENCES `opg01_users` (`id`),
@@ -50,27 +35,25 @@ CREATE TABLE `opg01_friends` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `opg01_friends` (`id`, `FK_user0`, `FK_user1`) VALUES
-(0,	1,	2),
-(1,	1,	0);
+(0,	0,	1),
+(1,	1,	2);
 
 DROP TABLE IF EXISTS `opg01_posts`;
 CREATE TABLE `opg01_posts` (
-  `id` int NOT NULL,
+  `id` int unsigned NOT NULL,
   `title` text NOT NULL,
   `content` text NOT NULL,
-  `FK_user` int NOT NULL,
+  `FK_user` int unsigned NOT NULL,
   UNIQUE KEY `id` (`id`),
   KEY `post_id` (`id`),
   KEY `FK_user` (`FK_user`),
   CONSTRAINT `opg01_posts_ibfk_1` FOREIGN KEY (`FK_user`) REFERENCES `opg01_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `opg01_posts` (`id`, `title`, `content`, `FK_user`) VALUES
-(0,	'post #0',	'hello and welcome to my programming tutorial\r\n\r\ntoday we will learn how to use rust',	0);
 
 DROP TABLE IF EXISTS `opg01_users`;
 CREATE TABLE `opg01_users` (
-  `id` int NOT NULL,
+  `id` int unsigned NOT NULL,
   `name` text NOT NULL,
   `permission` int NOT NULL,
   UNIQUE KEY `id` (`id`),
@@ -78,9 +61,5 @@ CREATE TABLE `opg01_users` (
   CONSTRAINT `opg01_users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `opg01_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `opg01_users` (`id`, `name`, `permission`) VALUES
-(0,	'dietz dieter',	1),
-(1,	'ole soelberg',	2),
-(2,	'ole helledie',	2);
 
--- 2022-05-04 12:36:17
+-- 2022-05-04 12:44:42
--- opg01.sql
+++ opg01.sql
@@ -10,3 +10,13 @@ SELECT users0.name, users1.name
 FROM opg01_friends
 JOIN opg01_users as users0 ON (opg01_friends.FK_user0 = users0.id)
 JOIN opg01_users as users1 ON (opg01_friends.FK_user1 = users1.id);
+
+-- drop view if exists
+DROP VIEW IF EXISTS named_friend_relations;
+
+-- create view that displays names of friends
+CREATE VIEW named_friend_relations AS
+SELECT users0.name as name0, users1.name as name1
+FROM opg01_friends
+JOIN opg01_users as users0 ON (opg01_friends.FK_user0 = users0.id)
+JOIN opg01_users as users1 ON (opg01_friends.FK_user1 = users1.id);
