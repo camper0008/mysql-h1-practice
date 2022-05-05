233fe668a41d598f6e92f4fc35ea5f4e3b92e2ea
camper0008 <tphollebeek@gmail.com>
2022-05-05 11:29:35 +0200

    move stuff

---
+++ str-opg-01/address.sql
@@ -0,0 +1,85 @@
+/*
+id=VARCHAR(36) UNIQUE PRIMARY KEY NOT NULL
+status=bit NOT NULL
+oprettet=timestamp NOT NULL
+ændret=timestamp NOT NULL
+vejkode=unsigned int NOT NULL 
+vejnavn=text NOT NULL
+addresseringsvejnavn=text NOT NULL
+husnr=text NOT NULL
+etage=text
+dør=text
+supplerendebynavn=text
+postnr=unsigned int NOT NULL
+postnrnavn=text NOT NULL
+stormodtagerpostnr=unsigned int
+stormodtagerpostnrnavn=text
+kommunekode=unsigned int NOT NULL
+kommunenavn=text NOT NULL
+ejerlavkode=unsigned int NOT NULL
+ejerlavnavn=text NOT NULL
+matrikelnr=TEXT NOT NULL
+esrejendomsnr=unsigned int NOT NULL
+etrs=89koordinat_øst=float NOT NULL
+etrs=89koordinat_nord=float NOT NULL
+wgs=84koordinat_bredde=float NOT NULL
+wgs=84koordinat_længde=float NOT NULL
+nøjagtighed=VARCHAR(1) NOT NULL
+kilde=unsigned int NOT NULL
+tekniskstandard=VARCHAR(2) NOT NULL
+tekstretning=float NOT NULL
+ddkn_m=100=text NOT NULL
+ddkn_km=1=text NOT NULL
+=10km_624_54=text NOT NULL
+adressepunktændringsdato=timestamp NOT NULL
+adgangsadresseid=VARCHAR(36) NOT NULL
+adgangsadresse_status=bit NOT NULL
+adgangsadresse_oprettet=timestamp NOT NULL
+adgangsadresse_ændret=timestamp NOT NULL
+regionskode=unsigned int NOT NULL
+regionsnavn=text NOT NULL
+jordstykke_ejerlavnavn=text NOT NULL
+kvhx=text NOT NULL
+sognekode=unsigned int NOT NULL
+sognenavn=text NOT NULL
+politikredskode=unsigned int NOT NULL
+politikredsnavn=text NOT NULL
+retskredskode=unsigned int NOT NULL
+retskredsnavn=text NOT NULL
+opstillingskredskode=unsigned int NOT NULL
+opstillingskredsnavn=text NOT NULL
+zone=text NOT NULL
+jordstykke_ejerlavkode=unsigned int NOT NULL
+jordstykke_matrikelnr=text NOT NULL
+jordstykke_esrejendomsnr=unsigned int NOT NULL
+kvh=text NOT NULL
+højde=float NOT NULL
+adgangspunktid=VARCHAR(36) NOT NULL
+vejpunkt_id=VARCHAR(36) NOT NULL
+vejpunkt_kilde=text NOT NULL
+vejpunkt_nøjagtighed=VARCHAR(1) NOT NULL
+vejpunkt_tekniskstandard=VARCHAR(2) NOT NULL
+vejpunkt_x=float NOT NULL
+vejpunkt_y=float NOT NULL
+afstemningsområdenummer=unsigned int NOT NULL
+afstemningsområdenavn=text NOT NULL
+brofast=bit NOT NULL
+supplerendebynavn_dagi_id=unsigned int
+navngivenvej_id=VARCHAR(36) NOT NULL
+menighedsrådsafstemningsområdenummer=unsigned int NOT NULL
+menighedsrådsafstemningsområdenavn=text NOT NULL
+vejpunkt_ændret=timestamp NOT NULL
+ikrafttrædelse=timestamp NOT NULL
+nedlagt=bit
+adgangsadresse_ikrafttrædelse=timestamp NOT NULL
+adgangsadresse_nedlagt=timestamp
+adgangsadresse_darstatus=unsigned int NOT NULL
+darstatus=unsigned int NOT NULL
+storkredsnummer=unsigned int NOT NULL
+storkredsnavn=text NOT NULL
+valglandsdelsbogstav=VARCHAR(1) NOT NULL
+valglandsdelsnavn=text NOT NULL
+landsdelsnuts3=text NOT NULL
+landsdelsnavn=text NOT NULL
+betegnelse=text NOT NULL
+*/
