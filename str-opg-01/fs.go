b09fd5cc42f14cdcbac537a29dceb3ada6296c73
piet <tphollebeek@gmail.com>
2022-05-08 20:01:48 +0200

    Update fs.go

--- str-opg-01/fs.go
+++ str-opg-01/fs.go
@@ -18,3 +18,19 @@ func ReadCSVHeader(filename string) string {
 	scanner.Scan()
 	return strings.TrimSpace(scanner.Text())
 }
+
+func ReadCSVEverythingElse(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // advance once to skip header
+	res := make([]string, 0)
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+	return res
+}
