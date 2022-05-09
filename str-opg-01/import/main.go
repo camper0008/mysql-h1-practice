e2c71ae0948815a41402105638aacc8f77a873a2
camper0008 <tphollebeek@gmail.com>
2022-05-09 13:08:22 +0000

    refactor+loading

--- str-opg-01/import/main.go
+++ str-opg-01/import/main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"csv-to-mysql/db_api"
 	db_utils "csv-to-mysql/db_api/utils"
+	"csv-to-mysql/fs"
 	"csv-to-mysql/table/defs"
 	"csv-to-mysql/utils"
 	"fmt"
 	"strings"
 )
 
-const FILENAME string = "./address.csv"
-const VALUES_PER_ITERATION int = 1000
-
 func ModifyHeader(filename string) []string {
-	rawHeader := ReadCSVHeader(filename)
+	rawHeader := fs.ReadCSVHeader(filename)
 
 	splitHeader := strings.Split(rawHeader, ",")
 	globalizedHeader := utils.DanishToEnglishFieldNames(splitHeader)
@@ -21,7 +19,7 @@ func ModifyHeader(filename string) []string {
 }
 
 func ModifyValues(filename string, modifiedHeaders []string) []string {
-	values := ReadCSVEverythingElse(filename)
+	values := fs.ReadCSVEverythingElse(filename)
 	types := defs.IntoTableQuoteType(modifiedHeaders)
 
 	res := []string{}
@@ -34,6 +32,9 @@ func ModifyValues(filename string, modifiedHeaders []string) []string {
 }
 
 func main() {
+	const FILENAME string = "./address.csv"
+	const VALUES_PER_ITERATION int = 1000
+
 	headers := ModifyHeader(FILENAME)
 	values := ModifyValues(FILENAME, headers)
 
@@ -44,15 +45,19 @@ func main() {
 	db_utils.InitializeDb(db, headers)
 	fmt.Println("done!")
 
+	fmt.Println("inserting values...")
 	for i := 0; i < len(values); i += VALUES_PER_ITERATION {
 		lowest := utils.Min(i+VALUES_PER_ITERATION, len(values))
 
 		db_utils.InsertInto(db, headers, values[i:lowest])
 
-		fmt.Printf("[%d / %d] working...\n", i, len(values))
+		percentage := float64(i) / float64(len(values)) * 100
+
+		fmt.Printf("%0.2f%% [%d / %d]\n", percentage, i, len(values))
 	}
 
-	fmt.Printf("[%d / %d] done!\n", len(values), len(values))
+	fmt.Printf("%0.2f%% [%d / %d]\n", 100.0, len(values), len(values))
+	fmt.Println("done!")
 
 	db.Close()
 }
